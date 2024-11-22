b2f0cb2d136c3bfbf63f6c0acdca733ec10f9440
minhquan <137802637+boybanggia123@users.noreply.github.com>
2024-11-22 15:38:58 +0700

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 // src/app/layout.jsx
 import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/header";
+import Footer from "./components/footer";
 import { Inter } from "next/font/google";
 import Providers from "../redux/Provider";
 import "../../public/bootstrap/css/bootstrap.css";
